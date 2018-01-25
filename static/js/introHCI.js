3e0cfdadc271cd8cd2f24e55d95a27f1cd49913a
Nicholas Wong <nic147919@gmail.com>
2018-01-25 02:02:02 -0800

    Finished lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -3,16 +3,31 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
-})
+});
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Experience tranquility");
+        $("#testjs").text("Please wait...");
+        $(".jumbotron p").addClass("active");
+        $("#testjs").toggleClass("active");        
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+    
+	$(".project").click(function(e) {
+        e.preventDefault();
+        var containingProject = $(this).closest(".project");
+        var description = $(containingProject).find(".project-description");
+        if (description.length == 0) {
+           $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+        } else {
+           //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+            description.fadeOut();
+        }
+    });
 }
